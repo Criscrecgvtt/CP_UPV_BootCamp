2804b8334fa7f48de99a4cc8db857acc696eaebe
Criscrecgvtt <crismendez.mir@gmail.com>
2025-07-02 13:53:26 +0200

    Capitulo 1 G

--- Capitulo_1/G_IgnoAme.cpp
+++ Capitulo_1/G_IgnoAme.cpp
@@ -5,11 +5,12 @@ int main(){
 double num;
 string text1;
 string line2;
-string line3;
+string palabraFinal;
 cin >>num>>text1;
 cin.ignore();
 getline(cin, line2);
-getline(cin, line3);
-cout<< line2+": "<< fixed << setprecision(3) <<num<<endl;
-cout << line3.at(0) << endl;
+cin>>palabraFinal;
+
+cout << line2 << ": " << fixed << setprecision(3) << num << endl;
+cout << palabraFinal << endl;
 }
